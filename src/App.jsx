8a1384155fcb1a0f6e76ea811ca8a08b452bbf4d
фывкеук <tvclash284@gmail.com>
2025-04-09 18:04:59 +0300

    This is a new commit for what I originally planned to be amended

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+      <p className="developer">
+        Developed by  <span>zerrez</span>
+      </p>
+    </div>
+  );
+};
+
+export default App;
